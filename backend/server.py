7a528814678e8309536d0e288561ddaea78b2f0a
lukedigiovanna <lukedigiovanna@gmail.com>
2023-10-15 19:42:25 -0400

    API endpoint shells for signing up/users

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 import atexit
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, abort, request
 import psycopg2
 from dotenv import load_dotenv
 import os
@@ -19,14 +19,54 @@ conn = psycopg2.connect(
     password=password
 )
 
+if conn == None:
+    print("FATAL ERROR: could not connect to the database")
+    exit()
+
 cursor = conn.cursor()
+
+if cursor == None:
+    print("FATAL ERROR: could not connect a cursor to the database")
+    exit()
 
 print("setting up flask app")
 app = Flask(__name__, static_folder="static")
 
+"""
+Returns the public HTML of the page
+"""
 @app.route('/')
-def hello():
+def root():
     return send_from_directory(app.static_folder, "index.html")
+
+"""
+Takes all user information necessary to sign the user up or uses
+default values when necessary.
+
+Will return an error status if any fields are not given
+"""
+@app.route('/api/signup', methods=["POST"])
+def signup():
+    return 'NOT IMPLEMENTED'
+
+"""
+Performs authentication to return a JWT that the client
+can then use for protected tasks
+"""
+@app.route('/api/login', methods=["POST"])
+def login():
+    return 'NOT_IMPLEMENTED'
+
+"""
+Gets a JSON array of all registered users
+"""
+@app.route('/api/users', methods=["GET"])
+def users():
+    # get up to 300 users
+    cursor.execute('SELECT * FROM user_account LIMIT 300 OFFSET 0')
+    result = cursor.fetchall()
+    return result
+
 
 # Safely close connections/resources when the server is shutdown for any reason
 def shutdown():
@@ -36,6 +76,6 @@ def shutdown():
 
 atexit.register(shutdown)
 
-
-app.run()
+if __name__ == "__main__":
+    app.run()
 
