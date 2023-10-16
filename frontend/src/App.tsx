3491b4ea70d475c6dd30e430838940d37f512050
lukedigiovanna <lukedigiovanna@gmail.com>
2023-10-16 17:47:52 -0400

    login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import { RegisterPage } from "./pages/RegisterPage";
 import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
 
 function App() {
     return (
         <div>
             <h1 className="flex justify-center items-center font-bold text-3xl text-gray-800 my-5">
-                <img src="vt-logo.png" className="w-14" /> Connect
+                <img src="vt-logo.png" alt="VT" className="w-14" /> Connect
             </h1>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
             </Routes>
         </div>
     );
--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -3,8 +3,11 @@ import { FormRow } from "./FormRow";
 export const LoginForm = () => {
     return (
         <form className="form">
-            <FormRow>
+            <FormRow title="VT PID">
                 <input type="text" placeholder="pid" className="form-input" />
+            </FormRow>
+            <FormRow title="Password">
+                <input type="text" placeholder="password" className="form-input" />
             </FormRow>
         </form>
     );
